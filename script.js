3df24b19ee818605add9a782e871ff493a39a9e4
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-11-19 12:29:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function upperCase(){
+	const thename= document.getElementById("fname");
+	thename.addEventListener(blur, function(){
+		thename.value=thename.value.toUpperCase()
+	});
+}
+upperCase();
